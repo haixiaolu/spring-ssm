d54ada0d85a5e8ba4cf24bd65b0fb7987b51f149
haixiaolu <haixiao.lu-1@ou.edu>
2022-11-23 22:19:45 -0600

    added GET and POST

---
+++ springmvc/springmvc_04_request_param/src/main/java/com/haixiaolu/config/SpringMvcConfig.java
@@ -0,0 +1,9 @@
+package com.haixiaolu.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.haixiaolu.controller")
+public class SpringMvcConfig {
+}
---
+++ springmvc/springmvc_04_request_param/src/main/java/com/haixiaolu/domain/Address.java
@@ -0,0 +1,22 @@
+package com.haixiaolu.domain;
+
+public class Address {
+    private String province;
+    private String city;
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
