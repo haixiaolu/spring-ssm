f260b59508f3ebddc702eed7c6d3ba4967f29018
haixiaolu <haixiao.lu-1@ou.edu>
2022-11-24 11:26:59 -0600

    added GET and POST use json data

--- springmvc/springmvc_04_request_param/src/main/java/com/haixiaolu/config/SpringMvcConfig.java
+++ springmvc/springmvc_04_request_param/src/main/java/com/haixiaolu/config/SpringMvcConfig.java
@@ -2,8 +2,10 @@ package com.haixiaolu.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan("com.haixiaolu.controller")
+@EnableWebMvc // 开启json数据类型自动转换
 public class SpringMvcConfig {
 }
--- springmvc/springmvc_04_request_param/src/main/java/com/haixiaolu/controller/UserController.java
+++ springmvc/springmvc_04_request_param/src/main/java/com/haixiaolu/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.haixiaolu.controller;
 
+import com.haixiaolu.domain.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 
@@ -14,5 +18,29 @@ public class UserController {
         System.out.println("普通参数传递 age ==> " +age);
 
         return "{'module: 'commonParam'}";
+    }
+
+    // 集合参数： json格式
+    @RequestMapping("/listparamForJson")
+    @ResponseBody
+    public String listparamForJson(@RequestBody List<String> likes){
+        System.out.println("list comm(json)参数传递 list ==> " +likes);
+        return "{'module' : 'list common for json param'}";
+    }
+
+    //POJO参数: json格式
+    @RequestMapping("/pojoParamForJson")
+    @ResponseBody
+    public String pojoParamForJson(@RequestBody User user){
+        System.out.println("pojo(json)参数传递 user ==> " + user);
+        return "{'module' : 'pojo for json param'}";
+    }
+
+    // 集合参数： json格式
+    @RequestMapping("/listPojoParamForJson")
+    @ResponseBody
+    public String listPojoParamForJson(@RequestBody List<User> list){
+        System.out.println("list pojo(json)参数传递 list ==> " +list);
+        return "{'module' : 'list pojo for json param'}";
     }
 }
