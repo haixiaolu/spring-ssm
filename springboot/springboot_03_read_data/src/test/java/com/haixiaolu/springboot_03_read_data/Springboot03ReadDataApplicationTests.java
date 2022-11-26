5b018cf42a2f9e7a09e432f595c6a1aecc541347
haixiaolu <haixiao.lu-1@ou.edu>
2022-11-26 10:46:31 -0600

    added springboot 读取配置数据

---
+++ springboot/springboot_03_read_data/src/test/java/com/haixiaolu/springboot_03_read_data/Springboot03ReadDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haixiaolu.springboot_03_read_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot03ReadDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
