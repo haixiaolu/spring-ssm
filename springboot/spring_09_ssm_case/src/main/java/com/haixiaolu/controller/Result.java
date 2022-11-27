5dadffca8ead4a89fd98cd59a5d62643103d9b79
haixiaolu <haixiao.lu-1@ou.edu>
2022-11-26 20:57:22 -0600

    added springboot 整合 ssm

---
+++ springboot/spring_09_ssm_case/src/main/java/com/haixiaolu/controller/Result.java
@@ -0,0 +1,63 @@
+package com.haixiaolu.controller;
+
+import com.haixiaolu.domain.Book;
+import com.haixiaolu.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class Result {
+    private Object data;
+    private Integer code;
+    private String msg;
+
+    public Result() {
+    }
+
+    public Result(Integer code, Object data) {
+        this.data = data;
+        this.code = code;
+    }
+
+    public Result(Integer code, Object data, String msg) {
+        this.data = data;
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "data=" + data +
+                ", code=" + code +
+                ", msg='" + msg + '\'' +
+                '}';
+    }
+}
---
+++ springboot/spring_09_ssm_case/src/main/java/com/haixiaolu/service/BookService.java
@@ -0,0 +1,20 @@
+package com.haixiaolu.service;
+
+import com.haixiaolu.domain.Book;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+public interface BookService {
+
+    public boolean save(Book book);
+
+    public boolean update(Book book);
+
+    public boolean delete (Integer id);
+
+    public Book getById(Integer id);
+
+    public List<Book> getAll();
+}
